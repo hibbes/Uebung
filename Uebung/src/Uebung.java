67e56ec983a62e2e4c8899c4c1f63dbb602694fd
Marek Czernohous <marek@czernohous.de>
2015-11-17 15:12:08 +0100

    SelectionSort

--- Uebung/src/Uebung.java
+++ Uebung/src/Uebung.java
@@ -2,24 +2,22 @@
 public class Uebung {
 
 	public static void main(String[] args) {
-		boolean[] zahlen = new boolean[160];   
-		for (int i = 2; i<160; i++){
+		int maxwert = 1024000;
+		int ausgabeinc = 0;
+		boolean[] zahlen = new boolean[maxwert];   
+		for (int i = 2; i<zahlen.length; i++){
 			if(zahlen[i]==false){
-				System.out.println("Für "+i);
-			  for(int j=i*2; j<160; j=j+i){
-				
+				for(int j=i*2; j<zahlen.length; j=j+i){
 					zahlen[j]=true;
-					System.out.print(j+"="+zahlen[j]+" ");
 				}
-				
 			  }
-			  System.out.println("");
-			  
 			}
-		
-		for (int i=0; i<160;i++){
+		for (int i=2; i<zahlen.length;i++){
 			if(!zahlen[i]){
 				System.out.print(i+" ");
+				ausgabeinc++;
+				if(ausgabeinc%10==0){System.out.println("");}
+				
 			}
 		}
 		
