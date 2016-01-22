5ceb0d6973faa8d594e70c6551f078c279272f95
Marek Czernohous <marek@czernohous.de>
2016-01-22 08:05:29 +0100

    Übung 1

--- Uebung/src/List.java
+++ Uebung/src/List.java
@@ -1,9 +1,24 @@
+import java.util.ArrayList;
+
 
 public class List {
-
+	
+			
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		ArrayList<String> liste = new ArrayList<String>();
+		liste.add("Deutsch");
+		liste.add("Englisch");
+		liste.add("Geographie");
+		
+		for(int i=0; i<liste.size(); i++){
+			System.out.println(liste.get(i));}
+		
+		liste.remove(0);
+		liste.add(0, "Leibesübungen");
 
+		for(int i=0; i<liste.size(); i++){
+			System.out.println(liste.get(i));}
+		
 	}
 
 }
