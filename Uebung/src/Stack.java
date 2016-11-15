ec5e29048518758c8ddb3300d4e4f9db22e96e5a
hibbes <marek@czernohous.de>
2016-11-15 13:30:44 +0100

    changes

--- Uebung/src/Stack.java
+++ Uebung/src/Stack.java
@@ -1,10 +1,37 @@
-
 public class Stack {
+
+	static int number=117;
 
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		for(int i =0; i<=1000;i++){
+			number=i;
+			System.out.println("");
+			System.out.print("n ="+number+": ");
+		while(number>1){
+		
+			if(number%2==0){
+			number=number/2;}
+		else{number=(number*3)+1;}
+		System.out.print(number +", ");	
+		}
 	}
 
+	}
+	
+	
+	public static boolean isPrime(int number){
+		if(number<2){return false;}
+		
+		for(int i=2; i<number/2;i++){
+		 			 
+		 if(number%i==0){
+			 System.out.println("Teiler:" +i);
+			return false;
+		 }
+		}
+		
+		return true;
+	}
+	
 }
