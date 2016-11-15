54d8b2d2e34df602abc47be3e3572db40f493850
Marek.Czernohous <Marek.Czernohous@SK-B4B52FAB64BE.2631.skool.net>
2016-11-15 13:30:44 +0100

    changes

--- Uebung/src/Arrays.java
+++ Uebung/src/Arrays.java
@@ -1,8 +1,71 @@
 
 public class Arrays {
 
+	static char [] array1 = new char[50];
+	static int i = 0;
+	static int j = 0;
+	
 	public static void main(String[] args) {
-		int[][] matrix = new int[3][3];
+
+		
+		
+		push('!');
+		push('d');
+		push('l');
+		push('r');
+		push('o');
+		push('W');
+		push(' ');
+		push('o');
+		push('l');
+		push('l');
+		push('e');
+		push('H');
+		
+		
+				
+		
+		while(!empty()){
+			System.out.print(pop());
+		}
+		
+		}
+	
+	
+		public static void push(char element){
+			array1[i]=element;
+			i++;
+			j++;
+		}
+		
+		public static char pop(){
+			
+			 
+			char element =	array1[j-i];
+			i--;
+		
+		return element;
+		  }
+		 
+	    public static boolean empty(){
+	    	if(i>0){return false;}
+	    		else{  	
+	    			return true;}
+	    	
+	    }
+	
+}	
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+	 * 	int[][] matrix = new int[3][3];
+	 
 		
 		matrix[0][0]=1;
 		matrix[0][1]=2;
@@ -25,4 +88,4 @@ public class Arrays {
 		
 }
 
-
+/*/
