99715929ee18fccf02845fbcd10639e80c676705
Marek.Czernohous <Marek.Czernohous@DESKTOP-JUVVB7M.2631.skool.net>
2018-10-04 09:04:36 +0200

    mm

--- Uebung/src/SelectionSort.java
+++ Uebung/src/SelectionSort.java
@@ -1,38 +1,27 @@
 
 public class SelectionSort {
+
 	
-	public static void main(String[] args) {
+	
+	public static void main(String[] args){
+		int[] list = {5, 8, 3, 6, 8, 9, 4, 2};
+		swap(list, list[0], list.length);
 		
-		int[] unsortiert = { 4, 1, 8, -3, 5, 7, 45, 7, 58, 6 ,5432, 6, 3, 987, 2, 1, -4, 66, 1337, 3497, 1674,432,433,8,7,341,243,909,101,1011 };
-		int[] sortiert = selectionsort(unsortiert);
-
-		}
-
-	public static int[] selectionsort(int[] sortieren) {
-		int inc = 0;
-		for (int i = 0; i < sortieren.length - 1; i++) {
-			for (int j = i + 1; j < sortieren.length; j++) {
-				inc++;
-				if (sortieren[i] > sortieren[j]) {
-					int temp = sortieren[i];
-					sortieren[i] = sortieren[j];
-					sortieren[j] = temp;
-					inc++;
-					
-					
-				}
-				
-			}
-		}
-		ausgabe(sortieren, inc);
-		return sortieren;
 	}
 	
-	private static void ausgabe(int[] sortieren, int inc){
-		System.out.println(inc+". Speicherzugriffe");
-		for(int i = 0; i<sortieren.length; i++){
-		System.out.print(sortieren[i]+", ");
-		}
-		System.out.println("");
+	public static void swap(int[] array, int position1, int position2) {
+ 
 	}
+
+	public static int findMinimum(int[] array, int startIndex, int endIndex) {
+
+return 0;
+
+}
+
+public static boolean isSorted(int[] array) {
+
+	return true;
+}
+
 }
