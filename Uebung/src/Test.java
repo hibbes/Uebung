fc51b002bd47622d243330347f9b2efb0e1f8c0a
MarekCzernohous <MarekCzernohous@0-209-L>
2022-09-16 10:19:29 +0200

    Queue und Stack

--- Uebung/src/Test.java
+++ Uebung/src/Test.java
@@ -4,58 +4,44 @@ public class Test {
 	public static void main(String[] args) {
 		ListenElement element1 = new ListenElement(4);
 		ListenElement element2 = new ListenElement(42);
-		Queue queue = new Queue(3);
-		queue.enqeue(5);
-		System.out.println("Größe: " + queue.getGroesse());
-		queue.enqeue(9);
-		System.out.println("Größe: " + queue.getGroesse());
-		try {queue.deqeue();
-		
-		} catch (Exception e) {
-
-			System.out.println("Der Stapel ist leer!");
-		}
-		
-	/*	try {
-			stack.top();
-		} catch (Exception e) {
-
-			System.out.println("Der Stapel ist leer!");
-		}
+		Queue myQueue = new Queue(3);
+		myQueue.enqueue(6);
+		System.out.println("Größe: " + myQueue.getGroesse());
 
 		try {
-			stack.pop();
+			myQueue.dequeue();
 		} catch (Exception e) {
 
-			System.out.println("Der Stapel ist leer!");
+			System.out.println("Die ist leer!");
 		}
 
-		System.out.println("Größe: " + stack.getGroesse());
-		try {
-			stack.pop();
-		} catch (Exception e) {
+		System.out.println("Größe: " + myQueue.getGroesse());
 
-			System.out.println("Der Stapel ist leer!");
-		}
-
-		try {
-			stack.pop();
-		} catch (Exception e) {
-
-			System.out.println("Der Stapel ist leer!");
-		}
-
-		try {
-			stack.top();
-		} catch (Exception e) {
-
-			System.out.println("Der Stapel ist leer!");
-		}
-
-		/*
-		 * Liste liste = new Liste(3); liste.add(5); liste.add(7); liste.add(999);
-		 * liste.add(1001); System.out.println(liste);
-		 * System.out.println(liste.getGroesse());
-		 */
 	}
 }
+/*
+ * try { stack.top(); } catch (Exception e) {
+ * 
+ * System.out.println("Der Stapel ist leer!"); }
+ * 
+ * try { stack.pop(); } catch (Exception e) {
+ * 
+ * System.out.println("Der Stapel ist leer!"); }
+ * 
+ * System.out.println("Größe: " + stack.getGroesse()); try { stack.pop(); }
+ * catch (Exception e) {
+ * 
+ * System.out.println("Der Stapel ist leer!"); }
+ * 
+ * try { stack.pop(); } catch (Exception e) {
+ * 
+ * System.out.println("Der Stapel ist leer!"); }
+ * 
+ * try { stack.top(); } catch (Exception e) {
+ * 
+ * System.out.println("Der Stapel ist leer!"); }
+ * 
+ * /* Liste liste = new Liste(3); liste.add(5); liste.add(7); liste.add(999);
+ * liste.add(1001); System.out.println(liste);
+ * System.out.println(liste.getGroesse());
+ */
