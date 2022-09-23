46f73fb175127d3c89e95afbf2fabb19552221f4
MarekCzernohous <MarekCzernohous@0-209-L>
2022-09-23 09:14:48 +0200

    BuildTree

--- Uebung/src/BuildTree.java
+++ Uebung/src/BuildTree.java
@@ -6,6 +6,12 @@ public class BuildTree
    {
       root = new Element(100);
       root.left = new Element(50);
-      root.right = new Element(150);   
+      root.right = new Element(150);  
+      root.insert(75);
+      
    }
+   
+   
+   
+   
 }
