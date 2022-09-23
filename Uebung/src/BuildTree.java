faedf1591df0de0388d1b56f992616ae0aa8d319
hibbes <m.czernohous@schiller-og.de>
2022-09-23 07:24:35 +0200

    Tree2

--- Uebung/src/BuildTree.java
+++ Uebung/src/BuildTree.java
@@ -1,4 +1,11 @@
+public class BuildTree
+{
+   Element root;
 
-public class BuildTree {
-
+   public BuildTree()
+   {
+      root = new Element(100);
+      root.left = new Element(50);
+      root.right = new Element(150);   
+   }
 }
