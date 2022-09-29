53dfc3245368053382e9d697033c9960a444519b
hibbes <marek@czernohous.de>
2022-09-29 12:59:56 +0200

    PQueue

---
+++ Uebung/src/BuildPQueue.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class BuildPQueue {
+
+	public static void main(String[] args) {
+		ArrayList<Notruf> pqueue = new ArrayList<Notruf>();
+
+	}
+
+	public void insert(ArrayList pqueue, Notruf notruf) {
+		pqueue.add(6, notruf);
+
+	}
+
+	public void delete() {
+	}
+
+//	public Element max() {
+//		return 0;
+//	}
+
+	public boolean empty() {
+		return true;
+	}
+
+}
---
+++ Uebung/src/PQueue.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class PQueue {
+	
+	
+	public PQueue() {
+		ArrayList<Notruf> pqueue = new ArrayList<Notruf>();
+	}
+	
+	
+	
+
+	
+}
