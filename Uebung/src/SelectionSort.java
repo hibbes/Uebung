feb25ac203ef2badd7479b75eeab051749fec913
hibbes <marek@czernohous.de>
2023-06-14 08:22:15 +0200

    Update

--- Uebung/src/SelectionSort.java
+++ Uebung/src/SelectionSort.java
@@ -1,27 +1,54 @@
+import java.util.Random;
 
-public class SelectionSort {
+public class SelectionSort {  
+static int howBig = 100;
+static long timeStart;
+static long timeEnd;
 
-	
-	
-	public static void main(String[] args){
-		int[] list = {5, 8, 3, 6, 8, 9, 4, 2};
-		swap(list, list[0], list.length);
+    public static int[] generateToSort(int n){
+        Random rand = new Random();
 		
-	}
-	
-	public static void swap(int[] array, int position1, int position2) {
- 
-	}
+		int[] numb = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			numb[i] = rand.nextInt(100000);
+		}
+        return numb;
+    }
 
-	public static int findMinimum(int[] array, int startIndex, int endIndex) {
-
-return 0;
-
-}
-
-public static boolean isSorted(int[] array) {
-
-	return true;
-}
-
-}
+    public static void selectionSort(int[] numb){  
+        timeStart = System.nanoTime(); 
+        for (int i = 0; i < numb.length - 1; i++)  
+        {  
+            int index = i;  
+            for (int j = i + 1; j < numb.length; j++){  
+                if (numb[j] < numb[index]){  
+                    index = j; //suche nach kleinstem Wert 
+                }  
+            }  
+            int smallerNumber = numb[index];   
+            numb[index] = numb[i];  
+            numb[i] = smallerNumber;  
+        } 
+        timeEnd = System.nanoTime(); 
+       
+		 
+    }  
+       
+    public static void main(String a[]){  
+        int[] toSort = generateToSort(howBig);
+        System.out.println("Vor der Sortierungt");  
+        for(int i: toSort){  
+            System.out.print(i+" ");  
+        }  
+        System.out.println();  
+          
+        selectionSort(toSort);//Sortierung  
+         
+        System.out.println("Nach der Sortierung");  
+        for(int i: toSort){  
+            System.out.print(i+" ");  
+        }
+        System.out.println("\n \nVerlaufszeit der Schleife: " + ((timeEnd - timeStart)/1000) + "ms.");   
+    }  
+}  
