67ac887f124409193bd42b341bfa673e5be31d78
hibbes <marek@czernohous.de>
2023-10-11 09:12:49 +0200

    Baum

--- Uebung/src/Baum.java
+++ Uebung/src/Baum.java
@@ -1,7 +1,25 @@
 public class Baum {
     public Knoten wurzel;
 
+    public Baum() {
+        this.wurzel = null;
+    }
+
     public Baum(Knoten knoten) {
         this.wurzel = knoten;
+    }
+
+    public int suche(int wert) {
+        return wurzel.suche(wert);
+    }
+
+    public static void main(String[] args) {
+        Baum b1 = new Baum(new Knoten(5));
+        b1.wurzel.linkesKind = new Knoten(3);
+        b1.wurzel.linkesKind.linkesKind = new Knoten(1);
+        b1.wurzel.rechtesKind = new Knoten(7);
+        b1.wurzel.rechtesKind.rechtesKind = new Knoten(11);
+        System.out.println(b1.suche(99));
+
     }
 }
