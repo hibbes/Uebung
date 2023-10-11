30d8b62aeb1b33e545341c9dde9ad0bba23b360a
hibbes <marek@czernohous.de>
2023-10-11 09:12:49 +0200

    Baum

--- Uebung/src/Knoten.java
+++ Uebung/src/Knoten.java
@@ -1,13 +1,36 @@
 public class Knoten {
-   private Knoten linkesKind;
-   private Knoten rechtesKind;
-   private int wert;
+   public Knoten linkesKind;
+   public Knoten rechtesKind;
+   public int wert;
 
-   public Knoten(int wert){     
-    this.wert=wert;
-    this.linkesKind = null;
-    this.rechtesKind = null;
+   public Knoten(int wert) {
+      this.wert = wert;
+      this.linkesKind = null;
+      this.rechtesKind = null;
    }
 
-   
+   public int suche(int wert) {
+
+      if (this.wert == wert) {
+         return this.wert;
+      } else {
+         if (this.wert > wert) {
+            if (this.linkesKind != null) {
+               return linkesKind.suche(wert);
+            } else {
+               return 0;
+            }
+         }
+
+         else {
+            if (this.rechtesKind != null) {
+               return rechtesKind.suche(wert);
+            } else {
+               return 0;
+            }
+         }
+
+      }
+
+   }
 }
